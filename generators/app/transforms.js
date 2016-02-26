8efc289ed7f96b0f59d947d8c02594b86fe68fc0
Matthieu Lux <matthieu.lux@gmail.com>
2016-02-26 17:54:17 +0100

    more generic transforms approach with default exports

--- generators/app/transforms.js
+++ generators/app/transforms.js
@@ -1,29 +1,24 @@
 'use strict';
 
-const path = require('path');
-
 module.exports = function transforms() {
-  this.replaceInFiles('src/**/*.{js,ts,tsx}', (content, fileName) => {
-    const baseName = path.basename(fileName, '.js');
-    const componentName = baseName.substr(0, 1).toUpperCase() + baseName.substr(1);
+  this.replaceInFiles('src/**/*.{js,ts,tsx}', content => {
     // remove es2015 webpack styles imports
     let result = content.replace(/import '.*ss';\n\n?/g, '');
     // remove commonjs webpack styles requires
     result = result.replace(/require\('.*ss'\);\n\n?/g, '');
-    // replace commonjs with es2015 lib imports
+    // replace commonjs with es2015 imports
     result = result.replace(
-      /var (.*) = require\(('[^\.].*')\);/g,
+      /var (.*) = require\(('.*')\);/g,
       'import $1 from $2;'
     );
-    // replace commonjs with es2015 local imports
     result = result.replace(
-      /var (.*) = require\(('\..*')\);/g,
-      'import { $1 } from $2;'
+      /require\(('.*')\);/g,
+      'import $1;'
     );
-    // replace commonjs with es2015 exports of createClass React components
+    // replace commonjs with es2015 exports
     result = result.replace(
-      /module\.exports = React\.createClass/,
-      `export const ${componentName} = React.createClass`
+      /module\.exports =/,
+      `export default`
     );
     // replace xhr urls to add src directory
     result = result.replace(
