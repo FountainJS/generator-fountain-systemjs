d6d47ecd8cafb2808fc9c701964d96c1a74313c3
Micaël <micael.mbagira@icloud.com>
2016-07-07 10:06:46 +0200

    Configuration of ng2test

--- generators/app/templates/jspm.test.js
+++ generators/app/templates/jspm.test.js
@@ -4,3 +4,17 @@ SystemJS.config({
     "npm:*": "/base/jspm_packages/npm/*"
   }
 });
+<% if (framework === 'angular2') { -%>
+SystemJS.import('jspm.config.js').then(() => {
+  return Promise.all([
+    System.import('@angular/core/testing'),
+    System.import('@angular/platform-browser-dynamic/testing')
+  ]).then(providers => {
+    const testing = providers[0];
+    const testingBrowser = providers[1];
+
+    testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+      testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+  });
+});
+<% } -%>
